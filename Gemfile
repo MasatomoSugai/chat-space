1584a6b2e3b80539be11d35581b3cf0fb4af86a8
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-07-09 15:31:24 +0900

    create API

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ gem 'font-awesome-rails'
 gem 'devise'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'jquery-ui-rails'
+gem 'pry-rails'
+
---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,61 @@
+$(function(){
+
+  function buildMessage(message, imageHTML){
+    var imageHTML = message.image.url ? `<img class="message__image" src="${message.image.url}" width=200 height=150 >` : "" 
+
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    ${message.content}
+                  </p>
+                  ${imageHTML}
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+  
+
+    var vallText = $('.new_message__text').val();
+    var vallImage = $('.hidden').val();
+    if(!vallText && !vallImage){
+      alert('メッセージを入力してください')
+      return false;
+    }
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message, imageHTML){
+      console.log(message)
+      var html = buildMessage(message, imageHTML);
+      $('.messages').append(html)
+      $('.new_message__text').val('')
+      $('.hidden').val('')
+      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+      $('.submit-btn').removeAttr('disabled');
+    })
+    .fail(function(){
+      alert('エラー');
+    })
+
+  })
+  
+
+});
