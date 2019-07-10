49d06d6a89d0cc4435fa860e6690dec070d7795e
Masatomo Sugai <tech.masatomo.s@gmail.com>
2019-07-10 13:27:49 +0900

    delete commentout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,26 +7,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    # binding.pry
 
     @message = @group.messages.create(message_params)
-    # @message.save
 
     respond_to do |format|
       format.html { redirect_to group_messages_path }
       format.json
     end
-    
-    # binding.pry
-    # if @message.save
-    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    # else
-    #   @message = @group.messages.includes(:user)
-    #   flash.now[:alert] = 'メッセージを入力してください！'
-    #   render :index
-    #   # render 'messages/index'
-    # end
-
   end
   
   private
